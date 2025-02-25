f57898cf9b4974d72110d27019b05086a0cdb3ea
K4rasu <46818080+K4rasu@users.noreply.github.com>
2025-02-25 18:35:25 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const popup = document.getElementById('popup');
     const overlay = document.getElementById('overlay');
+    const popup = document.getElementById('popup');
     const closeBtn = document.getElementById('close-btn');
 
+    // Pop-up anzeigen
     popup.style.display = 'block';
     overlay.style.display = 'block';
 
+    // Pop-up schließen
     closeBtn.addEventListener('click', function() {
         popup.style.display = 'none';
         overlay.style.display = 'none';
